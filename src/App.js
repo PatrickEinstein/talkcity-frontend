2982ab02851ea778c8bf1d1cdcd6cc9e864031d1
PatrickEinstein <mohammedola1234@gmail.com>
2023-02-25 04:21:29 +0100

    added redux, persist and material ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homepage";
+import LoginPage from "scenes/loginpage";
+import ProfilePage from "scenes/profilepage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import {CssBaseline, ThemeProvider} from '@mui/material';
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+
 
 function App() {
+  
+const mode = useSelector(( state) => (state.mode));
+const theme = useMemo( ( ) => createTheme(themeSettings(mode)), [mode]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <BrowserRouter>
+    <ThemeProvider theme ={theme}>
+      <CssBaseline />
+      <Routes>
+        <Route path ="/" element ={<LoginPage/>} />
+        <Route path ="/home" element ={<HomePage/>} />
+        <Route path ="/profile/:userId" element ={<ProfilePage/>} />
+      </Routes>
+      </ThemeProvider>
+    </BrowserRouter>    
     </div>
   );
 }
