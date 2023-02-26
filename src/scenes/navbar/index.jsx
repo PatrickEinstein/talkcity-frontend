2320c9d31587506ea16190e52622a289a8357038
PatrickEinstein <mohammedola1234@gmail.com>
2023-02-26 09:18:36 +0100

    made the login and register

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
   const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  // const user = useSelector((state) => state.user);
+// const user = useSelector((state) => state.user);
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
 
   const theme = useTheme();
