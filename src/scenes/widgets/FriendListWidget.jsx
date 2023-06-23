c0abac4725cba6f98823b944b0dc12206224542c
PatrickEinstein <mohammedola1234@gmail.com>
2023-06-23 06:08:33 -0400

    liveserver

--- src/scenes/widgets/FriendListWidget.jsx
+++ src/scenes/widgets/FriendListWidget.jsx
@@ -14,7 +14,7 @@ const FriendListWidget = ({ userId }) => {
   // const { userId} = useSelector((state) => (state.user));
   const getFriends = async () => {
     const response = await fetch(
-      `http://localhost:3001/users/${userId}/friends`,
+      `https://talkcity-backend-2l9h.vercel.app/users/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
